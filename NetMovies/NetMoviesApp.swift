793ca73d602d80d2391a1bef34a94c7ca06cf4cc
Abdulrahman Ibrahim <58425053+Ibra-sudo@users.noreply.github.com>
2023-04-26 00:59:19 +0200

    Initial Commit

---
+++ NetMovies/NetMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetMoviesApp.swift
+//  NetMovies
+//
+//  Created by Abdulrahman Ibrahim on 26.04.23.
+//
+
+import SwiftUI
+
+@main
+struct NetMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
